dcf010806abaa4a881d9f74fd6f241ab6d715cbf
Emilio <epelaaez@gmail.com>
2021-05-19 18:37:50 -0500

    add footer

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './App.css';
 
-import Header from '../Header/Header.js'
-import Main from '../Main/Main.js'
+import Header from '../Header/Header.js';
+import Main from '../Main/Main.js';
+import Footer from '../Footer/Footer.js';
 
 function App() {
   return (
     <div className="App">        
       <Header />
       <Main />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+    return (
+        <footer>
+            <div className="footer-bottom">
+                <a href="#privacy">Privacy Policy</a> • © 2021 Quantum Untangled
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
